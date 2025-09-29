a157cea69fa76beffc98a74d50c039cf6513d91d
jaykumarkota <jaykumarkota.official@gmail.com>
2025-09-29 16:46:46 +0530

    added the necessary change to deploy to gh-pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,12 @@ import './styles/modern-normalize.css'
 import './styles/index.css'
 import './styles/utils.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
